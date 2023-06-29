e77a61a6d06184e034b0c3f8ecae480a05a0de7c
William <you@example.com>
2023-06-29 15:18:41 +0200

    ajout possibilité tone text vide

---
+++ migrations/Version20230629131039.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230629131039 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE zone_texte CHANGE titre titre VARCHAR(100) DEFAULT NULL, CHANGE texte texte LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE zone_texte CHANGE titre titre VARCHAR(100) NOT NULL, CHANGE texte texte LONGTEXT NOT NULL');
+    }
+}
--- src/Form/ZoneTexteType.php
+++ src/Form/ZoneTexteType.php
@@ -6,14 +6,22 @@ use App\Entity\ZoneTexte;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ZoneTexteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('texte')
+            ->add('titre',TextType::class,[
+                    'required'=> false,
+                    'empty_data' => '',
+                ])
+            ->add('texte',TextareaType::class,[
+                    'required'=> false,
+                    'empty_data' => '',
+                ])
         ;
     }
 
