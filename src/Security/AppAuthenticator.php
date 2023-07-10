fa4c9def0d82e93ed63f9f86167727c7036888d6
William <you@example.com>
2023-07-10 11:58:33 +0200

    ajout validation modifier text/event

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -48,7 +48,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_menu'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
